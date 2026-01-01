d594c922ab26aa71c1ae21d32fec25c99eaf42d7
Mattia <mattia.millebolle@gmail.com>
2026-01-01 14:28:52 +0100

    token ewelink

---
+++ callback.py
@@ -0,0 +1,11 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/callback")
+def callback():
+    code = request.args.get("code")
+    return f"CODE RICEVUTO:<br><b>{code}</b>"
+
+if __name__ == "__main__":
+    app.run(port=5000)
