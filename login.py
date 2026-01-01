da94e6981935d15b08ceee13272d4ecea531b641
Mattia <mattia.millebolle@gmail.com>
2026-01-01 11:52:23 +0100

    login ewlink

--- login.py
+++ login.py
@@ -1,13 +1,20 @@
 import asyncio
 from ewelink import EWeLink
 
+APP_ID = "0ygkstU1AovdDXtI65mok181GCCY87EF"
+APP_SECRET = "0UZMVs2oaSoFiYcqtDM88hOTFLgLpZXu"
 EMAIL = "mattia.millebolle@gmail.com"
 PASSWORD = "Millebolle.1"
-REGION = "eu"
 
 async def main():
-    client = EWeLink(EMAIL, PASSWORD, REGION)
-    result = await client.login()
+    client = EWeLink(
+        appid=APP_ID,
+        appsecret=APP_SECRET,
+        region="eu"
+    )
+
+    result = await client.login(EMAIL, PASSWORD)
+    print("LOGIN OK:")
     print(result)
 
 if __name__ == "__main__":
